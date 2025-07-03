02538cffc18db19aeb7b2ed50e6532acfd578349
Nick Bolton <nick@symless.com>
2025-07-03 10:19:05 +0100

    feat: Implement caching for version response and add VSCode settings

--- cloudflare-worker/src/index.ts
+++ cloudflare-worker/src/index.ts
@@ -3,21 +3,35 @@
 
 import { Octokit } from '@octokit/rest';
 
+// With no API token, we can only make 60 requests per hour, but if we cache the response,
+// we can limit our requests to once every 2 minutes (which is 30 per hour).
+// We could limit this further to like 1 per hour, but this might confuse developers not aware
+// of the cache ("Why is the API version response not updating?")
+const cacheAgeSeconds = 60 * 2; // 2 mins (30 requests per hour)
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
+	async fetch(request: Request, _env: Env, _ctx: ExecutionContext): Promise<Response> {
 		const url = new URL(request.url);
 		if (url.pathname.startsWith('/version')) {
-			return await version(url);
+			return await version(request, url);
 		} else {
 			return new Response('Not found', { status: 404 });
 		}
 	},
 } satisfies ExportedHandler<Env>;
 
-async function version(url: URL) {
+async function version(request: Request, url: URL): Promise<Response> {
 	const fake = url.searchParams.get('fake');
 	if (fake) {
 		return new Response(fake);
+	}
+
+	const cache = caches.default;
+	const cacheKey = new Request(request);
+	const cachedResponse = await cache.match(cacheKey);
+	if (cachedResponse) {
+		console.debug('Cache hit for version');
+		return cachedResponse;
 	}
 
 	const octokit = new Octokit();
@@ -32,7 +46,14 @@ async function version(url: URL) {
 		const versionRaw = filteredReleases[0].tag_name;
 
 		// Backward compatibility: Strip any 'v' prefix, since the GUI doesn't expect one.
-		return new Response(versionRaw.replace(/^v/, ''));
+		const response = new Response(versionRaw.replace(/^v/, ''));
+
+		// Works with the cache code earlier in the function to cache the response next time.
+		console.debug(`Caching response for ${cacheAgeSeconds} seconds`);
+		response.headers.set('Cache-Control', `public, max-age=${cacheAgeSeconds}`);
+		await cache.put(cacheKey, response.clone());
+
+		return response;
 	} else {
 		return new Response('No valid release found', { status: 404 });
 	}
